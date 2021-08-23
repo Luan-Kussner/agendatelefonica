2091eace1c948fcb591d4b2d5b191510c28c3df7
Luan Kussner <luankussner@hotmail.com>
2021-08-23 16:00:50 -0300

    atualizando

--- index.php
+++ index.php
@@ -2,17 +2,40 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Agenda\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$page = new Page();
+
+	$page->setTpl("index");
     
-	$sql = new Agenda\DB\Sql();
+});
 
-	$results = $sql->select("SELECT * FROM tb_users");
+$app->get("/admin/contatos", function() {
 
-	echo json_encode($results);
+	$page = new PageAdmin();
+
+	$page->setTpl("contatos");
+});
+
+$app->get("/admin/contatos/:idcontato", function($idcontato) {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("contatos-create");
+});
+
+$app->get("/admin/contatos/:idcontato", function($idcontato) {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("contatos-update");
 });
 
 $app->run();
